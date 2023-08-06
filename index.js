ee2790b8a61dcf8c6385d002614478fce2196858
Ammar S.A.A <s.ammarahmed14@gmail.com>
2023-08-06 10:11:20 +0500

    FIX: CORS

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config(); // Load the environment variables from .env
@@ -6,6 +7,9 @@ dotenv.config(); // Load the environment variables from .env
 const port = process.env.SERVER_PORT || 3000;
 
 app.use(express.json());
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Define routes using the appropriate route handlers
 app.use('/api/users', require('./api/user/Routes'));
@@ -15,5 +19,4 @@ app.use('/api/product', require('./api/product/Routes'));
 
 app.listen(port, () => {
   console.log(`ShopSmart Server launched on port ${port}`);
-//   console.log('Environment variables:', process.env);
 });
